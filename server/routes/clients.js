74631b052156863b3c958d2bbefb6595c04d060f
Gadrawingz <legmeekmc4@gmail.com>
2024-09-10 09:57:43 +0200

    First commit for about and membership dummy content

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -11,6 +11,67 @@ router.get(['', '/home'], (req, res) => {
     res.render('clients/home', { internals });
 });
 
+
+// Routes for for about us
+router.get('/background', (req, res) => {
+    const internals = {
+        title: "Our Background",
+        description: ""
+    }
+    res.render('clients/about/background', { internals });
+});
+
+router.get('/psf-25', (req, res) => {
+    const internals = {
+        title: "PSF 25",
+        description: ""
+    }
+    res.render('clients/about/psf-25', { internals });
+});
+
+router.get('/strategies', (req, res) => {
+    const internals = {
+        title: "Strategies",
+        description: ""
+    }
+    res.render('clients/about/strategies', { internals });
+});
+
+
+// Routes for Membership
+router.get('/association', (req, res) => {
+    const internals = {
+        title: "Clusters - Association",
+        description: ""
+    }
+    res.render('clients/membership/assoc', { internals });
+});
+
+router.get('/golden-circle', (req, res) => {
+    const internals = {
+        title: "Golden Circle",
+        description: ""
+    }
+    res.render('clients/membership/golden-circle', { internals });
+});
+
+router.get('/regular', (req, res) => {
+    const internals = {
+        title: "Regular",
+        description: ""
+    }
+    res.render('clients/membership/regular', { internals });
+});
+
+
+
+
+
+
+
+
+
+
 // Services
 router.get('/services', (req, res) => {
     const internals = {
